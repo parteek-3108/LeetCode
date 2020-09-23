43918177b514a2fc0af181080b462ed1aac01c4b
parteek-3108 <parteekad03@gmail.com>
2020-09-23 11:14:08 +0530

    938 Sum of BST

---
+++ 938 Sum of BST.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root == null) return 0;
+        if(root.val > R) return rangeSumBST(root.left, L, R);
+        if(root.val < L) return rangeSumBST(root.right, L, R);
+        return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);      
+    }
+}
+// class Solution {
+//     int sum=0;
+//     public int rangeSumBST(TreeNode root, int L, int R) {
+//         if(root==null)
+//             return sum;
+       
+//             if(root.val>=L && root.val<=R)
+//          sum+=root.val;
+//         rangeSumBST(root.left,L,R);
+//         rangeSumBST(root.right,L,R);
+//         return sum;
+//     }
+//     }
