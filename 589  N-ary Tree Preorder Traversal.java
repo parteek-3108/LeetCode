daac797365b09b75d3493fd0f4345e8d0bd40281
parteek-3108 <parteekad03@gmail.com>
2020-10-09 11:57:30 +0530

    589  N-ary Tree Preorder Traversal

---
+++ 589  N-ary Tree Preorder Traversal.java
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+import java.util.*; 
+import java.util.stream.*; 
+class Solution {
+    List<Integer> x=new ArrayList<Integer>();
+    public List<Integer> preorder(Node root) {
+        if(root!=null)
+        {
+           x.add(root.val);
+            for(int i=0;i<root.children.size();i++)
+            preorder(root.children.get(i));
+        }
+      return x;      
+    }
+}
