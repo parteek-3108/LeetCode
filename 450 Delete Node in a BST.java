abf032f7ef7dcfec9c8eb47321c5f9a169240e34
parteek-3108 <parteekad03@gmail.com>
2020-10-17 12:12:06 +0530

    450 Delete Node in a BST

---
+++ 450 Delete Node in a BST.java
@@ -0,0 +1,30 @@
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return root;
+        }
+        if (root.val == key) {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode min = findMin(root.right);
+                root.val = min.val;
+                root.right = deleteNode(root.right, min.val);
+            }
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            root.left = deleteNode(root.left, key);
+        }
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+}
