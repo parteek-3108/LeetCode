dab5603d09d0fc05c5ba76104f6a99237853de64
parteek-3108 <parteekad03@gmail.com>
2020-10-22 12:19:36 +0530

    389 Find the Difference

---
+++ 389 Find the Difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+		int sumS = 0, sumT = 0;
+		char[] arrS = s.toCharArray();
+		char[] arrT = t.toCharArray();
+		for (int i = 0; i < arrS.length; i++) {
+			sumS += arrS[i] - 'a';
+			sumT += arrT[i] - 'a';
+		}
+		sumT += arrT[arrT.length - 1] - 'a';
+		return (char) (sumT - sumS + 'a');
+	}
+}
