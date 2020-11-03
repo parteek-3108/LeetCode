4f4291383ff18c95f17f4d22950c284688f5ae1b
parteek-3108 <parteekad03@gmail.com>
2020-11-03 12:35:07 +0530

    198 House Robber

---
+++ 198 House Robber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {  
+    if(nums.length==0) return 0;
+    if(nums.length==1) return nums[0];
+	int[] mark = new int[nums.length];
+	mark[0] = nums[0];
+	mark[1] = Math.max(nums[0], nums[1]);
+	for(int i=2;i<nums.length;i++){
+		mark[i] = Math.max(nums[i]+mark[i-2], mark[i-1]);
+	}
+	return mark[nums.length-1];
+}
+}
