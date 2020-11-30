a56f0f8d9aab4d4660f7cd70071ed9d46a6bcf9b
parteek-3108 <parteekad03@gmail.com>
2020-11-30 13:10:54 +0530

    1296 Divide Array in Sets of k consecutive numbers

---
+++ 1296 Divide Array in Sets of k consecutive numbers.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isPossibleDivide(int[] hand, int W) {
+        
+        if(hand.length%W!=0)
+            return false;
+        Arrays.sort(hand);
+        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        List <Integer> list=new ArrayList<Integer>();
+        list.add(hand[0]);
+        map.put(hand[0],1);
+        for(int i=1;i<hand.length;i++)
+        {
+           if(hand[i]!=hand[i-1])
+               list.add(hand[i]);
+                
+            map.put(hand[i],map.getOrDefault(hand[i],0)+1);
+            
+        }
+        // for(int i=0;i<list.size();i++)
+        //     System.out.print(list.get(i)+" ");
+        for(int i=0;i<list.size();i++)
+        {
+            int count=map.get(list.get(i));
+            int index=i;
+            int check=0;
+            int limit=list.get(i)+W-1;
+            while(index<list.size() && list.get(index)<=limit)
+            {
+                if(map.get(list.get(index))>count && check==0)
+                {i=index-1;check=1;}
+                if(map.get(list.get(index))<count)
+                    return false;
+                map.put(list.get(index),map.get(list.get(index))-count);
+                index++;
+            }
+            if((check==1 && index>=list.size()) || (list.get(index-1)<limit))
+                return false;
+           
+            if(check==0)
+                i=index-1;
+        }
+        return true;
+    }
+}
